12e62064b89130cd85e90e9fe67475a3af44c2dc
Lascau Ionut Sebastian <lascauionutsebastian98@gmail.com>
2019-12-04 11:03:50 +0200

    first

---
+++ DAY4/PART2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int cnt = 0;
+	for (char firstDigit = '2'; firstDigit <= '6'; firstDigit++) {
+		for (char secondDigit = firstDigit == '2'? '8' : firstDigit; secondDigit <= '9'; secondDigit++) {
+			for (char thirdDigit = secondDigit; thirdDigit <= '9'; thirdDigit++) {
+				for (char fourthDigit = thirdDigit; fourthDigit <= '9'; fourthDigit++) {
+					for (char fifthDigit = fourthDigit; fifthDigit <= '9'; fifthDigit++) {
+						for (char sixthDigit = fifthDigit; sixthDigit <= '9'; sixthDigit++) {
+							if ((firstDigit == secondDigit || secondDigit == thirdDigit || thirdDigit == fourthDigit || fourthDigit == fifthDigit || fifthDigit == sixthDigit) &&
+								((sixthDigit == fifthDigit && fifthDigit != fourthDigit) ||
+								 (firstDigit == secondDigit && secondDigit != thirdDigit) ||
+								 (secondDigit == thirdDigit && firstDigit != secondDigit && thirdDigit != fourthDigit) ||
+								 (thirdDigit == fourthDigit && secondDigit != thirdDigit && fourthDigit != fifthDigit) ||
+								 (fourthDigit == fifthDigit && thirdDigit != fourthDigit && fifthDigit != sixthDigit))) {
+								++cnt;
+							}
+						}
+					}	
+				}
+			}
+		}
+	}
+	cout << cnt;
+	return 0;
+}
